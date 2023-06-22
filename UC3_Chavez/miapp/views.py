7847c7580768c9ba1eaf525a5f5dff96adb70e52
Jefersson Rodrigo Anaya Castro <20A3010118@untels.edu.pe>
2023-06-22 16:49:31 -0500

    UC3_Parte03_ANAYA_CASTRO

--- UC3_Chavez/miapp/views.py
+++ UC3_Chavez/miapp/views.py
@@ -27,20 +27,29 @@ def inicio(request):
         'cursos' : cursos
     })
 
+    
 def primos(request):
-    a=10
-    b=20
-    resultado=f"""
-            <h2>Números de [{a}, {b}] </h2>
-            Resultado: <br>
-            <ul>    
-            """
-    while a<=b:
-        resultado += f"<li>{a}</li>"
-        a+=1
-    resultado += "</ul>"
-    return HttpResponse(layaut + primos)
+    # Aquí colocaremos la lógica para encontrar los números primos
+    prime_numbers = []
+    count = 0
+    number = 2
 
+    while count < 10:  # Cambia este número si deseas imprimir más números primos
+        is_prime = True
+
+        # Verificar si el número es primo
+        for i in range(2, number):
+            if number % i == 0:
+                is_prime = False
+                break
+
+        if is_prime:
+            prime_numbers.append(number)
+            count += 1
+
+        number += 1
+
+    return HttpResponse(layaut + prime_numbers )
 def examen(request):
 
     return render(request, 'examen.html')
